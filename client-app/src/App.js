ee97cb5fdf399b4cac3302615af35bef4fdb2fb9
Vitalii Kyktov <vitalii.kyktov@dev-pro.net>
2017-04-13 23:02:11 +0300

    display main app field

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -6,6 +6,8 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {deepOrange500} from 'material-ui/styles/colors';
 
+import Paper from 'material-ui/Paper';
+
 const muiTheme = getMuiTheme({
     palette: {
         accent1Color: deepOrange500,
@@ -16,15 +18,8 @@ class App extends Component {
     render() {
         return (
             <MuiThemeProvider muiTheme={muiTheme}>
-                <div className="App">
-                    <div className="App-header">
-                        <img src={logo} className="App-logo" alt="logo"/>
-                        <h2>Welcome to React</h2>
-                    </div>
-                    <p className="App-intro">
-                        To get started, edit <code>src/App.js</code> and save to reload.
-                    </p>
-                </div>
+                <Paper zDepth={1} className="App">
+                </Paper>
             </MuiThemeProvider>
         );
     }
